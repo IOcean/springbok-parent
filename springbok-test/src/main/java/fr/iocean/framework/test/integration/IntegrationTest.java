df9e689956877af880fceaa9d2174feed335c387
Clément Tamisier <ctamisier@iocean.fr>
2016-05-03 14:02:37 +0200

    setDefaultLocale to french

--- springbok-test/src/main/java/fr/iocean/framework/test/integration/IntegrationTest.java
+++ springbok-test/src/main/java/fr/iocean/framework/test/integration/IntegrationTest.java
@@ -1,11 +1,14 @@
 package fr.iocean.framework.test.integration;
 
+import java.util.Locale;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.testng.AbstractTransactionalTestNGSpringContextTests;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 
 /**
@@ -13,14 +16,19 @@ import org.testng.annotations.BeforeMethod;
  */
 @WebAppConfiguration
 public abstract class IntegrationTest extends AbstractTransactionalTestNGSpringContextTests {
-    
-    @Autowired 
+
+    @Autowired
     protected WebApplicationContext webApplicationContext;
-    
+
     protected MockMvc mockMvc;
-    
+
     @BeforeMethod
     protected void setUpMethod() {
         mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @BeforeClass
+    protected void setUpLocale() {
+        Locale.setDefault(Locale.FRANCE);
     }
 }
